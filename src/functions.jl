30939da0d7f22aba1b951342de77a26067267867
Coy Zimmermann <czimmermann@mines.edu>
2022-08-23 17:03:47 -0600

    add a doctest

--- src/functions.jl
+++ src/functions.jl
@@ -9,10 +9,15 @@ end
     super_function(x, y)
 
 Compute `2x + 3y + 3`.
+
+# Example
+```jldoctest
+julia> super_function(3,2)
+15
+```
 """
 function super_function(x, y)
     p1 = my_f(x, y)
 
     add_three(p1)
 end
-
