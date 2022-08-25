3fc106e2f04fc70b652196a435b6ba5e0622acbb
Coy Zimmermann <czimmermann@mines.edu>
2022-08-25 12:29:04 -0600

    enable doc build workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,6 @@
 using Documenter, CoyLearnsPackageDev
 
-makedocs(sitename="CoyLearnsPackageDev.jl", format = Documenter.HTML(prettyurls = false))
+DocMeta.setdocmeta!(CoyLearnsPackageDev, :DocTestSetup, :(using CoyLearnsPackageDev); recursive=true)
+makedocs(sitename="CoyLearnsPackageDev.jl", format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
+
+deploydocs(repo = "github.com/czimm79/CoyLearnsPackageDev.jl.git")
